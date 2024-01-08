4f1de187ff84615ac3e4d2ce450805bf4ef91e57
Marcelo Filipov <marcelo.filipov@gmail.com>
2024-01-08 20:21:57 -0300

    @Before e @After

--- src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -3,25 +3,37 @@ package br.com.alura.leilao.acceptance.steps;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
 import org.junit.Assert;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class PropondoLanceSteps {
 
     private Lance lance;
     private Leilao leilao;
-    private Lance lance10;
-    private Lance lance15;
+    private ArrayList<Lance> lista;
+
+    @Before
+    public void setup() {
+        this.lista = new ArrayList<Lance>();
+        leilao = new Leilao("Tablet XPTO");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("after");
+    }
 
     @Dado("um lance valido")
     public void dado_um_lance_valido() {
         Usuario usuario = new Usuario("fulano");
         lance = new Lance(usuario, BigDecimal.TEN);
-        leilao = new Leilao("Tablet XPTO");
     }
 
     @Quando("propoe ao leilao")
@@ -48,22 +60,20 @@ public class PropondoLanceSteps {
 
     @Dado("um lance de {double} reais do usuario {string}")
     public void um_lance_de_reais_do_usuario_fulano(Double valor, String nomeUsuario) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Lance lance = new Lance(new Usuario(nomeUsuario), new BigDecimal(valor));
+        lista.add(lance);
     }
 
     @Quando("propoe varios lances ao leilao")
     public void propoe_varios_lances_ao_leilao() {
-        leilao.propoe(lance10);
-        leilao.propoe(lance15);
+        this.lista.forEach(lance -> leilao.propoe(lance));
     }
 
     @Entao("os lances sao aceitos")
     public void os_lances_sao_aceitos() {
-        Assert.assertEquals(2, leilao.getLances().size());
-        Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
-        Assert.assertEquals(new BigDecimal(15.0), leilao.getLances().get(1).getValor());
-
+        Assert.assertEquals(this.lista.size(), leilao.getLances().size());
+        Assert.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(0).getValor());
+        Assert.assertEquals(this.lista.get(1).getValor(), leilao.getLances().get(1).getValor());
     }
 
 }
